a2e121eb37b9995ec9ad4dd3db5a483b17920d0c
Just1nA <jadamso2@vols.utk.edu>
2020-04-10 16:04:44 -0400

    Test Files

--- Bubble Shooter Project/Assets/Scripts/GridManager.cs
+++ Bubble Shooter Project/Assets/Scripts/GridManager.cs
@@ -4,20 +4,56 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public GameObject[,] grid;
-    public static int ROW = 10;
-    public static int COL = 10;
-       
+    public GameObject[] bubbles;
+    public Sprite sprite;
+    private int index = 0;
+    //public float[,] grid;
+    //public static int ROW = 10;
+    //public static int COL = 9;
+    int Vertical, Horizontal, Columns, Rows;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        Vertical = (int)Camera.main.orthographicSize;
+        Horizontal = (int)(Vertical * Camera.main.aspect);
+        Columns = Horizontal * 2;
+        Rows = Vertical * 2;
+        //grid = new float[Columns, Rows];
 
+        
+
+        for (int i = 10; i < Columns; ++i)
+        {
+            for (int j = 10; j < Rows; ++j)
+            {
+                index = getRandom();
+                //GameObject referenceTile = (GameObject)bubbles[index];
+                GameObject tile = (GameObject)Instantiate(bubbles[index], transform);
+                tile.transform.position = new Vector2(i - (Horizontal - 0.5f) ,  j - (Vertical - 0.5f));
+                //grid[i, j] = Random.Range(0.0f, 1.0f); 
+                //GenerateGrid(i , j, grid[i, j]);
+            }
+        }
+           
     }
 
-    // Update is called once per frame
-    void Update()
+/*
+    private void GenerateGrid(int x, int y, float value)
     {
-        
+       //GameObject g = (GameObject)Instantiate(Resources.Load("Bubble"));
+       GameObject g = new GameObject("X: "+x+"Y: "+y);
+       g.transform.position = new Vector3(x - (Horizontal - 0.5f), y - (Vertical - 0.5f));
+       var s = g.AddComponent<SpriteRenderer>();
+       s.sprite = sprite;
+       s.color = new Color(value, value, value); 
+    }
+*/
+
+    private int getRandom()
+    {
+        int randomNum = Random.Range(0, 5);
+        return randomNum;
     }
 }
