f4d16fe4208b062f532619fdf65dea96fcfde605
swhite83 <59886244+swhite83@users.noreply.github.com>
2020-04-11 15:14:55 -0500

    ok

---
+++ Bubble Shooter Project/Assets/SideWalls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+   void OnTriggerEnter2D (Collider2D col) 
+    {
+    
+        
+    
+        Bubble.swallHit = true;
+        Bubble.rotating = false;
+        Debug.Log(Bubble.swallHit);
+        Debug.Log("SMALL");
+        if(col.tag == "Bubble")
+        {
+            //Debug.Log("Function");
+           // col.GetComponent<Bubble>().rb.velocity = Vector3.zero;
+           col.GetComponent<Bubble>().rb.gravityScale = 0;
+            var tempx = transform.position.x;
+            var tempy = transform.position.y;
+           // tempz = tempz + tempz;
+            tempx = tempx + tempx;
+            col.GetComponent<Bubble>().rb.velocity = new Vector2(-tempx, -tempy);
+            
+            //swallHit = false;
+        }
+        
+    }
+}
